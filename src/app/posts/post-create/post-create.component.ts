53565b81a88fd025ebb45ace597e6e005f124e34
nelsonjunior <nelsonjunior.developer@gmail.com>
2021-09-04 10:26:08 +0200

    Creates a service in order to make sharing posts among modules simpler

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
+  @Output() postCreatedEvent = new EventEmitter<Post>();
 
-  enteredValue = '';
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
 
-  newPost = 'NO CONTENT';
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content,
+    };
 
-  onAddPost() {
-
-    console.log('testing...');
-
-    this.newPost = this.enteredValue;
-
+    //Creates an event to be listened in the parent component trough the @Output annotation in the property
+    this.postCreatedEvent.emit(post);
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,17 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, Input } from '@angular/core';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
-
+  styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
+  // posts = [
+  //     {title: 'First Post!', content: 'This is the first post\s content'},
+  //     {title: 'Second Post!', content: 'This is the second post\s content'},
+  //     {title: 'Third Post!', content: 'This is the third post\s content'},
+  // ];
 
-  posts = [
-      {title: 'First Post!', content: 'This is the first post\s content'},
-      {title: 'Second Post!', content: 'This is the second post\s content'},
-      {title: 'Third Post!', content: 'This is the third post\s content'},
-  ];
+  //Adds the @input decorator in order to mae the posts property bindable outside
+  //By default it is not bindable
+  //We use the Input decorator because we want to bind some value from the outside (only from parent component)
+  @Input() posts: Post[] = [];
 
+  constructor(public postsService: PostsService) {}
 }
